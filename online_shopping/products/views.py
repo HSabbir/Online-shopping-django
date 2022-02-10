4263f74b1f52d7d8d416f4dd69ee3abc12974089
sabbir <bsse1014@iit.du.ac.bd>
2022-02-11 01:15:37 +0600

    added all product to home pages

--- online_shopping/products/views.py
+++ online_shopping/products/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    #print(products[0].image)
+    return render(request, 'index.html',{'products':products})
