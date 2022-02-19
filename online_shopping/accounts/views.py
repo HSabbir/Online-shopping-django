01762e93654b63a03bad459a65db333a79c30696
sabbir <bsse1014@iit.du.ac.bd>
2022-02-19 09:11:19 +0600

    login logout done

--- online_shopping/accounts/views.py
+++ online_shopping/accounts/views.py
@@ -13,3 +13,24 @@ def register_view(request):
 
     context = {"form":form}
     return render(request, 'account/register.html', context)
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user_obj = form.get_user()
+            login(request,user_obj)
+            return redirect('home')
+    else:
+        form = AuthenticationForm(request)
+
+    context = {"form":form}
+    return render(request, 'account/login.html',context)
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('logout')
+    return render(request, 'account/logout.html',{})
